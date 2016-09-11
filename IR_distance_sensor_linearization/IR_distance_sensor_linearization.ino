b9f773b9c184e225a382fa98d29b3123e2d31487
Radially <jonathanfriedman2@gmail.com>
2016-09-11 14:15:43 +0300

    Add files via upload

---
+++ IR_distance_sensor_linearization/IR_distance_sensor_linearization.ino
@@ -0,0 +1,27 @@
+/*  The method below linearize the input values of the distance sensor GP2D12
+ *  The method get the (int)value and return (double)distance in cm
+ *  Theory www.cs.uml.edu/teams-academy/uploads/Robots/Nonlinear.pdf
+ *  Code by J.M.Friedman
+ */
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  double sensorVal = analogRead(0); // Read sensor value.
+  Serial.println(linearIRS(sensorVal));
+  delay(100);
+}
+
+double linearIRS(int sensorValue) {
+  //theory  www.cs.uml.edu/teams-academy/uploads/Robots/Nonlinear.pdf
+  double a = 3;
+  double b = 8;
+  double c = 0;
+  double k = 18500;
+  double delta = sensorVal - c;
+  double result = (k - delta) / (a * delta);
+  return result;
+}
+
